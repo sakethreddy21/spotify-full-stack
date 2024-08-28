21f1f0e8d967c3ebc3b4a6816af78d908e1de8aa
saketh <saketh.reddy@trainingmug.com>
2024-08-28 21:41:50 +0530

    vercel deployment

--- spotify-backend/src/index.js
+++ spotify-backend/src/index.js
@@ -23,7 +23,7 @@ app.use(cors())
 app.use("/api/song", songRouter )
 app.use("/api/album", albumRouter )
 
-app.get("/", (req, res) => res.send("API Working"))
+app.get("/", (req, res) => res.json("API Working"))
 
 app.listen(port, () => console.log(`Server started on ${port}`))
 
